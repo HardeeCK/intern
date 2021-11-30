6c474d81053a07251fbc1b8d3b3cb0faed4d42cb
HardeeCK <hardeekhambhla@gmail.com>
2021-11-30 23:27:34 +0530

    Cover page with Cover image and content added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Cover from "./components/Cover";
+import CoverContent from "./components/CoverContent";
 
 function App() {
   return (
     <Router>
       <Navbar/>
+      <Cover/>
+      <CoverContent/>
     </Router>
   );
 }
---
+++ src/components/Cover.js
@@ -0,0 +1,10 @@
+
+function Cover(){
+    return (
+    <div className="cover">
+        <img className="cover-image" src="./cover-image.jpg"></img>
+    </div>
+    );
+}
+
+export default Cover;
